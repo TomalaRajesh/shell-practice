3b9fbaca20cb06610f4f0adc91a4421e479760ab
tomalarajesh <tomalaraj@gmail.com>
2025-06-14 23:13:39 +0530

    special vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -5,6 +5,7 @@ echo "Number of variables: $#"
 echo "Script name: $0"
 echo "Current Directory: $PWD"
 echo "User running this script: $USER"
-echo Home directory of user : $HomE"
+echo "HOME directory user : $HomE"
 echo "PID of the script: $$"
-sleep 10 & 
+sleep 10 &
+echo "PID of last command in background: $!"
